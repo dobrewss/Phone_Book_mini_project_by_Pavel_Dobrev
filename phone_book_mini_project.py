9b78a896b2539ba2326f8b0edafc6142edeb44eb
Pavel Dobrev <pavel.dobrev@icloud.com>
2023-04-02 18:43:13 +0300

    Search option added. Working on optimization.

--- phone_book_mini_project.py
+++ phone_book_mini_project.py
@@ -4,6 +4,7 @@ contacts = []
 
 
 while True:
+    print("Please add contact:")
     name = input()
     number = input()
     address = input()
@@ -15,8 +16,17 @@ while True:
         'E-mail': email
     }
     contacts.append(CONTACT_INFO)
+    question = input("Do you want to search contact? ")
+    if question.lower() == "yes":
+        search = input("Search contact: ")
+        searched_contact = len(name[::])
+        if len(search) == searched_contact:
+            print(f"Contact found - {search} - {CONTACT_INFO['Number']} - "
+                  f"{CONTACT_INFO['Address']} - {CONTACT_INFO['E-mail']}")
+        else:
+            print(f"No contacts found.")
+            pass
     MAX_CONTACT -= 1
     if MAX_CONTACT == 0:
         print(f"ALL CONTACTS ADDED: \n{contacts}")
         break
-
